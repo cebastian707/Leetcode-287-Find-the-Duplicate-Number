865fd50c40b22cd157d487c587c40288de0808c0
Cebastian Santiago Hernandez <57859731+cebastian707@users.noreply.github.com>
2022-05-27 11:08:50 -0700

    Add project files.

---
+++ Find the Duplicate Number/main.cpp
@@ -0,0 +1,37 @@
+/*By::Cebastian Santiago
+* Leetcode 287
+*/
+#include<iostream>
+#include<vector>
+#include<map>
+
+int findDuplicate(std::vector<int>& nums) {
+	//variables 
+	std::map<int, int> Hash;
+
+	//loop through the vector
+	for (size_t i = 0; i < nums.size(); i++){
+		//check to see if we've seen this number
+		if (Hash.find(nums[i]) == Hash.end()) {
+			//add this to the hash map 
+			Hash.insert({ nums[i],1 });
+		}
+
+		else {
+			return nums[i];
+		}
+
+	}
+	return 0;
+}
+
+
+
+
+int main(int argv, char argc[]) {
+	std::vector<int> num = { 3,1,3,4,2 };
+
+	std::cout << findDuplicate(num) << std::endl;
+
+	return 0;
+}
